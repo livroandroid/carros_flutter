37c7ec7fbbfebaa609ae8d1f909344d430f13ba7
rlechetalive <qw100781>
2019-01-29 16:28:54 -0200

    Prefs idx

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,32 +1,67 @@
 import 'package:carros/domain/carro.dart';
-import 'package:carros/domain/services/carro_service.dart';
-import 'package:carros/utils/nav.dart';
+import 'package:carros/utils/prefs.dart';
 import 'package:carros/widgets/carros_listView.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+
+  TabController tabController;
+
+  @override
+  initState()  {
+    super.initState();
+
+    tabController = TabController(length: 3, vsync: this);
+
+    Prefs.getInt("tabIndex").then((idx){
+      tabController.index = idx;
+    });
+
+    tabController.addListener(() async {
+      int idx = tabController.index;
+
+      Prefs.setInt("tabIndex",idx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Carros"),
-          bottom: TabBar(tabs: [
-            Tab(text: "Clássicos",icon: Icon(Icons.directions_car),),
-            Tab(text: "Esportivos",icon: Icon(Icons.directions_car),),
-            Tab(text: "Luxo",icon: Icon(Icons.directions_car),),
-
-          ]),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+        bottom: TabBar(
+          controller: tabController,
+          tabs: [
+            Tab(
+              text: "Clássicos",
+              icon: Icon(Icons.directions_car),
+            ),
+            Tab(
+              text: "Esportivos",
+              icon: Icon(Icons.directions_car),
+            ),
+            Tab(
+              text: "Luxo",
+              icon: Icon(Icons.directions_car),
+            ),
+          ],
         ),
-        body: TabBarView(children: [
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [
           CarrosListView(TipoCarro.classicos),
           CarrosListView(TipoCarro.esportivos),
-          CarrosListView(TipoCarro.luxo)
-        ]),
+          CarrosListView(TipoCarro.luxo),
+        ],
       ),
     );
   }
-
-
 }
