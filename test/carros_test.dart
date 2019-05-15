f4fc8f735f141c0be9047663fdca220013a7553b
rlecheta <rlecheta@livetouch.com.br>
2019-05-15 16:34:44 -0300

    google-services

---
+++ test/carros_test.dart
@@ -0,0 +1,21 @@
+
+import 'package:carros/domain/carro.dart';
+import 'package:carros/domain/services/carro_service.dart';
+import 'package:flutter/services.dart';
+import 'package:test/test.dart';
+
+
+void main() {
+  test('Carros All Test', () async {
+    List<Carro> carros = await CarroService.getCarros();
+    expect(carros.length > 0, true);
+
+    expect(carros.length, 34);
+  });
+
+  test('Carro Ferrari', () async {
+    Carro c = await CarroService.getCarro(11);
+    expect("Ferrari FF", c.nome);
+  });
+
+}
